19692ac63629d5275a198f8025fafadb48ba6af9
Susana vik <susana.m.vik@gmail.com>
2021-02-14 17:29:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,13 +21,12 @@ def get_english_meaning(path, emoticon)
   emoticon_hash = load_library(path)
   emoticon_hash.each do | key, translation |
     if emoticon == translation[:japanese]
-      binding.pry
+      key
+    else
+      'Sorry, that emoticon was not found'
     end
   end
 
-  english_meaning =  emoticon_hash(emoticon)
-    key
-  english_meaning ? english_meaning : 'Sorry, that emoticon was not found'
 end
 
 def get_japanese_emoticon(path, emoticon)
