c53a3214f503cbc12eb24e0de6336cc5d764668b
Susana vik <susana.m.vik@gmail.com>
2021-02-14 16:33:16 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,7 +21,7 @@ def get_english_meaning(path, emoticon)
   emoticon_hash = load_library(path)
   emoticon_hash.each do | key, language |
     language.each { |key1, emoticon| emoticon }
-  #  binding.pry
+    binding.pry
   if emoticon_hash.keys?(emoticon)
     key
   else return "Sorry, that emoticon was not found"
